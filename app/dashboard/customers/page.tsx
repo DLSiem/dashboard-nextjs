e2744fb0efa6d53ac4bc1666f6236ec3d9c38a6a
DLSiem <pdlsiem17@gmail.com>
2024-03-19 22:25:26 +0530

    complete customer page

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,27 @@
-export default function Page() {
-  return <p>Customer Page</p>;
+import CustomersTable from '@/app/ui/customers/table';
+import { lusitana } from '@/app/ui/fonts';
+import Search from '@/app/ui/search';
+import { fetchCustomersPages } from '@/app/lib/data';
+import { Suspense } from 'react';
+import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
+import Pagination from '@/app/ui/invoices/pagination';
+
+export default async function Page({
+  searchParams,
+}: {
+  searchParams?: { query?: string; page?: string };
+}) {
+  const query = searchParams?.query || '';
+  const currentPage = Number(searchParams?.page) || 1;
+  const totalPages = await fetchCustomersPages(query);
+  return (
+    <div className="w-full">
+      <Suspense key={query + currentPage} fallback={<InvoicesTableSkeleton />}>
+        <CustomersTable query={query} currentPage={currentPage} />
+      </Suspense>
+      <div className="mt-5 flex w-full justify-center">
+        <Pagination totalPages={totalPages} />
+      </div>
+    </div>
+  );
 }
